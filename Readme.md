3a2d56d88271e6802e1280774d489bf926820629
hugo borini <hborini@localhost.localdomain>
2020-01-14 03:45:08 -0500

    update the echo message and the Readme

--- Readme.md
+++ Readme.md
@@ -1,10 +1,10 @@
-# Borini Hugo
+# Init-repo-scss
 
 ## Script who helps to init your repo with sccs/parcel
 
 ### Usage :
 
-#### 1. Move the "file init-repo-scss" in your empty repo
+#### 1. Move the file "init-repo-scss" in your empty repo
 
 #### 2. Type : ./init-repo-scss.sh in your terminal
 
--- init-repo-scss.sh
+++ init-repo-scss.sh
@@ -2,7 +2,10 @@
 
 npm init -y
 
+clear
+echo -en '\E[47;32m'"\033[1m initialing NPM \n \033[0m"   # Vert
 touch .gitignore
+echo -en '\E[47;32m'"\033[1m Add the gitigniore and igniore all the trash file\033[0m"   # Vert
 
 echo "/node_modules
 /dist
@@ -10,13 +13,17 @@ echo "/node_modules
 /.DS_Store
 /init-repo-scss.sh " > .gitignore
 
+
 npm install parcel-bundler --save-dev
 
+clear
+
+echo -en '\E[47;32m'"\033[1m Create the src folder with style folder js folder and the index.html\033[0m"   # Vert
 
 mkdir src
 touch src/index.html
 echo "<!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
   <head>
     <meta charset='UTF-8' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
@@ -35,3 +42,4 @@ mkdir src/js
 touch src/js/app.js
 sed -i '7i\ "dev": "parcel src/index.html",\n' package.json
 sed -i '8i\ "build": "parcel build src/index.html",\n' package.json
+clear
